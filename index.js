a90259267e70d4bc67292f42ba6b87ab3f00cb92
Rashid Mohammed Iliyas Khan <khanr7433@gmail.com>
2025-11-02 14:39:24 +0530

    set up Express server with CORS and JSON parsing middleware

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
