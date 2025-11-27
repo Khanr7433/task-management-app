eb8ff867666dea9d674acbcb71c463ffdf6b650c
Rashid Mohammed Iliyas Khan <khanr7433@gmail.com>
2025-11-27 18:02:06 +0530

    Feat: Implement POST /api/tasks to create a task

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -3,4 +3,27 @@ import { Task } from "../models/Task.js";
 
 const router = express.Router();
 
+// Create a Task
+router.post("/", async (req, res) => {
+    try {
+        const { title, description, status, dueDate } = req.body;
+
+        if (!title) {
+            return res.status(400).json({ message: "Title is required" });
+        }
+
+        const newTask = new Task({
+            title,
+            description,
+            status,
+            dueDate,
+        });
+
+        const savedTask = await newTask.save();
+        res.status(201).json(savedTask);
+    } catch (error) {
+        res.status(500).json({ message: "Server Error", error: error.message });
+    }
+});
+
 export default router;
